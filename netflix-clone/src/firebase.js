6076a527df90e1ca39a5755e49b45c0319d83cdd
HappyCodingMama <happycodingmama@gmail.com>
2021-02-19 23:20:30 +0900

    setup firebase & hosting_API

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADwnYvumj0ki7qA_c82K6AFMWiPgDOn2E",
+  authDomain: "netflix-clone-c5091.firebaseapp.com",
+  projectId: "netflix-clone-c5091",
+  storageBucket: "netflix-clone-c5091.appspot.com",
+  messagingSenderId: "311500068473",
+  appId: "1:311500068473:web:9536fe8e65fe92c081e6a8",
+  measurementId: "G-TDS4S33X6Q",
+};
