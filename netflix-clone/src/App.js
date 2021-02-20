4c36bee3bb549c0385a3ab781bbeb023676e0324
HappyCodingMama <happycodingmama@gmail.com>
2021-02-21 02:20:12 +0900

    update posters

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Row from "./Row";
+import requests from "./requests";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>Start!</h1>
-      <Row title="Netflix Original" />
-      <Row title="Trending Now" />
+      <Row title="Netflix Original" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 import "./Row.css";
 
-function Row() {
-  return <div className="row"></div>;
+const base_url = "http://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
+      {/* container -> posters */}
+    </div>
+  );
 }
 
 export default Row;
