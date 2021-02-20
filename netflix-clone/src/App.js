c57b6275ddd0cd1b841ca17c822be32d854b6aaf
HappyCodingMama <happycodingmama@gmail.com>
2021-02-20 23:22:22 +0900

    build the Row components

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Row from "./Row";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>Start!</h1>
+      <Row title="Netflix Original" />
+      <Row title="Trending Now" />
     </div>
   );
 }
