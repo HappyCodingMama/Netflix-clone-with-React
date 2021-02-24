30908c29c19702e5a48b60ccdec394372410ab58
HappyCodingMama <happycodingmama@gmail.com>
2021-02-24 23:03:46 +0900

    Update App.js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -9,6 +9,12 @@ function App() {
       <h1>Start!</h1>
       <Row title="Netflix Original" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Tob Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documetaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
