a0b12dd1e7d1ff1e297db2f4c8fa70d60aaf1a50
HappyCodingMama <happycodingmama@gmail.com>
2021-02-27 23:00:45 +0900

    build the Banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>Start!</h1>
+      <Banner />
       <Row
         title="Netflix Original"
         fetchUrl={requests.fetchNetflixOriginals}
