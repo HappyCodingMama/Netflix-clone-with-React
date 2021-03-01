b1d6c35b3e4316fd70ab2c2f6b149b82b737560b
HappyCodingMama <happycodingmama@gmail.com>
2021-03-01 23:04:32 +0900

    Build Nav-Bar

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -19,6 +19,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -39,10 +43,12 @@ function Banner() {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
-        {/* div > 2 buttons */}
-        {/* description */}
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+
+      <div className="banner__fadeBottom"></div>
     </header>
   );
 }
